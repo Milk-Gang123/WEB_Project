2a841968799e4b55f0d8fafd0d498fa161bfe0d6
Администратор <alexushveridze@mail.ru>
2022-03-20 19:52:46 +0300

    Pixel art added

---
+++ Pixelart.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import time
+
+
+def make_pixel_art(file, pixel_size):
+        time_ = time.ctime(0)
+        image = Image.open(file)
+        image_width, image_height = image.size
+        image = image.resize((image_width // pixel_size, image_height // pixel_size), Image.NEAREST)
+        image = image.resize((image_width, image_height),Image.NEAREST)
+        image.save('static/img/processed_image_path.png')
+
+
+make_pixel_art('static/img/img.png', 80)
